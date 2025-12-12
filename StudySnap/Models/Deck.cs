d9521ff004ef7c390b6cddf5b778e3d882c9320e
Meter Games <118026542+metergames@users.noreply.github.com>
2025-12-11 20:28:48 -0500

    Refactor Deck class with properties and methods

--- StudySnap/Models/Deck.cs
+++ StudySnap/Models/Deck.cs
@@ -4,7 +4,83 @@
  */
 namespace StudySnap.Models
 {
-    class Deck
+    /// <summary>
+    /// Collection of flashcards, grouped.
+    /// </summary>
+    public class Deck
     {
+        private string _name;
+        private List<Flashcard> _cards;
+
+        /// <summary>
+        /// Name of the deck. Cannot be empty.
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Deck name cannot be empty.");
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// List of flashcards contained in deck.
+        /// Setting property to null will replace it with an empty list, so deck never stores null values.
+        /// </summary>
+        public List<Flashcard> Cards
+        {
+            get { return _cards; }
+            set
+            {
+                if (value == null)
+                    _cards = new List<Flashcard>();
+                else _cards = value;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new deck with the specified name and an empty flashcard list.
+        /// The name is required and the deck is created ready for editing and adding flashcards.
+        /// </summary>
+        /// <param name="name">Deck name</param>
+        public Deck(string name)
+        {
+            Name = name;
+            Cards = new List<Flashcard>();
+        }
+
+        /// <summary>
+        /// Adds a new flashcard to the deck.
+        /// </summary>
+        /// <param name="card">Flashcard object to add</param>
+        /// <exception cref="ArgumentException">If card to add is null</exception>
+        public void AddCard(Flashcard card)
+        {
+            if (card == null)
+                throw new ArgumentException("Cannot add a null card to the deck.");
+            Cards.Add(card);
+        }
+
+        /// <summary>
+        /// Removes a flashcard from the deck.
+        /// </summary>
+        /// <param name="card">Flashcard object to remove</param>
+        public void RemoveCard(Flashcard card)
+        {
+            if (card != null)
+                Cards.Remove(card);
+        }
+
+        /// <summary>
+        /// Returns total number of cards in the deck.
+        /// </summary>
+        /// <returns>Integer for count of cards</returns>
+        public int GetCardCount()
+        {
+            return Cards.Count;
+        }
     }
 }
